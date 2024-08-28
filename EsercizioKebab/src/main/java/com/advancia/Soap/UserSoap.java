a8b7a13976c156f1bf8b27a99cad312efc8eb248
FrancescoGiordano23 <francesco.giordano@advancia.it>
2024-08-28 17:13:44 +0200

    SOAP SII

---
+++ EsercizioKebab/src/main/java/com/advancia/Soap/UserSoap.java
@@ -0,0 +1,38 @@
+package com.advancia.Soap;
+
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.xml.bind.annotation.XmlElement;
+
+import com.advancia.Modal.User;
+import com.advancia.Utility.UserServices;
+
+@WebService(serviceName = "UserSoap")
+public class UserSoap {
+
+	@WebMethod(operationName = "getUserById")
+	public User getUserId(@WebParam(name = "id") Integer id) throws Exception {
+		return UserServices.getUserById(id);
+	}
+	@WebMethod(operationName = "getAllUsers")
+	public List<User> getAllUsers() throws Exception {
+		return UserServices.getAllUsers();
+	}
+
+	@WebMethod(operationName = "addUser")
+	public void adduser(
+	@WebParam(name = "username") @XmlElement(name="username",required=true, nillable=false) String username,
+	@WebParam(name = "password") @XmlElement(name="password", required=true, nillable=false) String password
+	    ) throws Exception {
+	        UserServices.createUser(username, password);
+	}
+	
+	@WebMethod(operationName = "deleteuser")
+    public void deleteMuser(@WebParam(name = "id") Integer id) throws Exception {
+        UserServices.deleteUserById(id);
+}
+	
+}
